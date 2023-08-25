5dec637b242d7801bbc11dc7d0df9b925d1d545e
Abdelrhman111111 <Abdelrhmanmohamed.m14@gmail.com>
2023-08-25 04:48:54 -0700

    done

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# making  file in /tmp with specific content
+
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
